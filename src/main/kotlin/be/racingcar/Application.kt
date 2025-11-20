1cfa93527afa7fc97e791a0e1f9ac57bab40fbf2
kangrae <kangrae0501@gmail.com>
2025-11-20 17:21:40 +0900

    refactor: 서비스 로직 이전 및 스프링부트 시작점 추가

--- src/main/kotlin/be/racingcar/Application.kt
+++ src/main/kotlin/be/racingcar/Application.kt
@@ -1,23 +1,11 @@
 package be.racingcar
 
-import be.racingcar.controller.RaceController
-import be.racingcar.domain.car.Cars
-import be.racingcar.domain.random.Dice
-import be.racingcar.domain.round.Round
-import be.racingcar.util.Console
-import be.racingcar.view.InputView
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
-fun main() {
+@SpringBootApplication
+class Application
 
-    val names = InputView.readCarNames()
-    val cars = Cars(names)
-
-    val totalRound = InputView.readTotalRound()
-    val round = Round(totalRound)
-    Console.close()
-
-    val controller = RaceController(Dice(), cars, round)
-    controller.startRace()
-    controller.endRace()
-
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
